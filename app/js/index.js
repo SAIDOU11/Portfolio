cfce5ed2de16979d331adef75d8f5b6174a7a6cb
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-04-26 10:11:49 +0200

    nav open & close

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,13 @@
-// toggleTheme()
+const navOpen = document.querySelector(".nav-open");
+const navClose = document.querySelector(".nav-close");
+const nav = document.querySelector(".nav");
+
+navOpen.addEventListener("click", () => {
+  console.log("open");
+  nav.classList.add("open-navigation");
+});
+
+navClose.addEventListener("click", () => {
+  console.log("close");
+  nav.classList.remove("open-navigation");
+});
